2c7d4501a8cbe11ac7befd52f5a1866bad3119f1
Jason Smith <SmiJa@users.noreply.github.com>
2021-08-10 19:10:56 -0700

    added a lot more to the HW

--- day01/js/main.js
+++ day01/js/main.js
@@ -1,12 +1,23 @@
 // This is a single line Comment
 
 /*
-
 A multiline Comment
-
 */
 
 console.log("Name: Jason Smith");
 
-console.log("Favorite food: Pizza")
+console.log("Favorite food: Pizza");
 
+const colorPicker = document.querySelector("#color-picker");
+const body = document.querySelector("body");
+const btn = document.querySelector("button");
+
+
+btn.addEventListener("click", changeBGColor);
+
+function changeBGColor () {
+    body.style.backgroundColor = colorPicker.value;
+    btn.style.borderColor = colorPicker.value;
+    btn.style.color = colorPicker.value;
+    console.log(colorPicker.value);
+}
