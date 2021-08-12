cfa1d74639fde3cb4d1d80e134eb3ecdc96e155e
Jason Smith <SmiJa@users.noreply.github.com>
2021-08-12 00:45:05 -0700

    created todo.js and laid out the HTML and started working on the styling

---
+++ day02/js/main.js
@@ -0,0 +1,10 @@
+const longWord = "Supercalifragilisticexpialidocious";
+const bigNum = 13n;
+
+console.log(longWord.length);
+
+console.log(1313);
+
+console.log(bigNum);
+
+console.log(typeof bigNum)
