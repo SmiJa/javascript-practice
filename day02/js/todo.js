8584c9929ce0caa49677433bf29b245222737797
Jason Smith <SmiJa@users.noreply.github.com>
2021-08-13 17:48:07 -0700

    Removed JS that didn't work. Simplified the JS to be more clear

--- day02/js/todo.js
+++ day02/js/todo.js
@@ -2,22 +2,33 @@ const taskInput = document.querySelector("#task-input");
 const btn = document.querySelector("button");
 const list = document.querySelector("#list");
 
-function buildItem(text) {
-    let listItem = 
-    `<li>
-        <div class="item">
-            <p class="item-text">${text}</p>
-        </div>
-    </li>`;
+// function buildItem(text) {
+//     let listItem = 
+//     `<li>
+//         <div class="item">
+//             <p class="item-text">${text}</p>
+//         </div>
+//     </li>`;
 
-    return listItem;
-}
+//     return listItem;
+// }
 
 function addItem() {
     let newItem = taskInput.value;
-    console.log(newItem);
-    return list.innerHTML = buildItem(newItem);
-}
+    let listItem = 
+    `<li>
+        <div class="item">
+            <p class="item-text">${newItem}</p>
+        </div>
+    </li>`;
 
+    if (newItem === "") {
+        alert("You didn't enter anything.");
+    } else {
+        list.innerHTML += listItem;
+    }
+    
+    taskInput.value = "";
+}
 
 btn.addEventListener("click", addItem);
