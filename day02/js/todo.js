eb694cc82cb210e06bb1caaed36c59e10d252649
Jason Smith <SmiJa@users.noreply.github.com>
2021-08-14 01:00:01 -0700

    Removed the filler li tag from the index. Added focus to the button

--- day02/js/todo.js
+++ day02/js/todo.js
@@ -2,17 +2,6 @@ const taskInput = document.querySelector("#task-input");
 const btn = document.querySelector("button");
 const list = document.querySelector("#list");
 
-// function buildItem(text) {
-//     let listItem = 
-//     `<li>
-//         <div class="item">
-//             <p class="item-text">${text}</p>
-//         </div>
-//     </li>`;
-
-//     return listItem;
-// }
-
 function addItem() {
     let newItem = taskInput.value;
     let listItem = 
